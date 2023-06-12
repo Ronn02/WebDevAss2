272cfb7d9166c6be6c533bcc9ac0d9ab363eca64
Ronn02 <72732177+Ronn02@users.noreply.github.com>
2023-06-12 18:51:26 +0800

    hatdog

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Content1 from "./components/Content1";
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Content1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Content1.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import img1 from '../assets/mainImg1.png'
+import img2 from '../assets/mainImg2.png'
+const Content1 = () => {
+  return (
+    <div className='flex flex-col md:flex-row justify-center items-center h-screen  mt-[250px] md:mt-[-100px] '>
+      <div className='mx-[10px] md:mx-[-10px] my-[20px] md:my-[0px]'>
+            <h1 className='font-semibold text-6xl p-10 text-center'>STUDIO GHIBLI</h1>
+            <p className='text-center font-normal text-3xl'>-Exquisite Exhibition:
+                <br />
+                <span className='italic'>Artistry of Studio Ghibli</span>
+                <br />
+                <br />
+                Artworks
+            </p>
+      </div>
+      <div  className='mx-[20px] my-[20px] md:my-[0px]'>
+        <img src={img1} alt="" className='h-[400px]' />
+      </div>
+      <div  className='mx-[20px] my-[20px] md:my-[0px]'>
+        <img src={img2} alt="" className='h-[400px]' />
+      </div>
+    </div>
+  );
+};
+
+export default Content1;
+
