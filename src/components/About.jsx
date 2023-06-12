6a1f8c31ca7f5a6b778fd934260b07bb5ab72e7b
Ronn02 <72732177+Ronn02@users.noreply.github.com>
2023-06-12 21:17:56 +0800

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className='flex justify-center items-center'>
-        <div className='w-[800px]'>
-            <hr />
+    <div className='flex justify-center items-center mt-[300px] md:mt-[50px]'>
+        <div className='mx-[20px] md:mx-[0px] md:w-[800px]'>
+            <hr className='border-t border-black my-[50px]' />
       <p> Studio Ghibli is known for its stunning and iconic artwork in its animated films. 
                 Their attention to detail, strong use of color, and emphasis on character design have
                  made their films beloved by audiences around the world. Many of their films feature stunning 
                  landscapes and backgrounds that are often inspired by real-world locations in Japan, adding to 
                  the authenticity and beauty of their artwork.</p>
-                 <hr />
+                 <hr className='border-t border-black my-[50px]' />
         </div>
     </div>
   );
